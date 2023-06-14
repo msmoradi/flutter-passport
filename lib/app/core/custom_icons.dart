dabfe5768d22239a83c49f1bf27bbe0dd7d7a307
msmoradi <mr.ms.moradi@gmail.com>
2023-06-15 00:15:21 +0330

    Add custom icons

---
+++ lib/app/core/custom_icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData cup = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData people = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData group_47 = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
