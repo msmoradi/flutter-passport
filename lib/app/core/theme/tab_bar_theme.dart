89ecc2c4a17726cf36c1e3dfc9df5c53c36f19f1
msmoradi <mr.ms.moradi@gmail.com>
2023-06-16 02:11:37 +0330

    Add appBarTheme

---
+++ lib/app/core/theme/tab_bar_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PassportTabBarTheme {
+  static TabBarTheme lightTabBarTheme = const TabBarTheme(
+    indicatorSize: TabBarIndicatorSize.tab,
+    labelColor: Colors.green,
+    unselectedLabelColor: Colors.grey,
+  );
+}
