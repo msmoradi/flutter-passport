e63aac6fd29d09075c90c4dfb05137baacf57b2a
msmoradi <mr.ms.moradi@gmail.com>
2023-06-16 00:07:26 +0330

    Add fa localizationsDelegates

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/route_manager.dart';
+import 'package:passport/app/core/theme/app_bar_theme.dart';
+import 'package:passport/app/core/theme/tab_bar_theme.dart';
 import 'package:passport/app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
       title: "Flutter Application",
+      localizationsDelegates: const [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("fa", "IR"),
+      ],
+      locale: const Locale("fa", "IR"),
+      theme: ThemeData(
+          useMaterial3: true,
+          tabBarTheme: PassportTabBarTheme.lightTabBarTheme,
+          appBarTheme: PassportAppBarTheme.lightAppBarTheme,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green)),
       getPages: AppPages.routes,
       initialRoute: AppPages.initial,
     ),
