aa3dccbb3a24421d3810ee2519667b871ed1b80b
msmoradi <mr.ms.moradi@gmail.com>
2023-06-16 02:12:10 +0330

    Add themes

---
+++ lib/app/core/theme/app_bar_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PassportAppBarTheme {
+  static AppBarTheme lightAppBarTheme = const AppBarTheme(
+    backgroundColor: Colors.white,
+    centerTitle: true,
+  );
+}
